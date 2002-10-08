1106b5674dca39fef9aae89b7569b47e17e97cfe
David Schleef <ds@schleef.org>
2002-10-08 02:23:00 +0000

    reverting last checkin.  It was supposed to go to a local repository, but I had failed to modify CVS/Root.
    Original commit message from CVS:
    reverting last checkin.  It was supposed to go to a local repository,
    but I had failed to modify CVS/Root.

--- gst-plugin/autogen.sh
+++ gst-plugin/autogen.sh
@@ -8,7 +8,7 @@ fi
 
 # if you would want to be running autoheader as well, you will have to do
 # something similar as above for it
-if test -z $AUTOCONF; then export AUTOCONF=autoconf2.50; fi
+if test -z $AUTOCONF; then export AUTOCONF=autoconf-2.53; fi
 set -x
 
 # if any of these steps fails, the others will not execute, which is good
