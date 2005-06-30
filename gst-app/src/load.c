a8f4fd9737d14826302124fbf2b0d9f96a8c3541
Ronald S. Bultje <rbultje@ronald.bitfreak.net>
2005-06-30 16:13:28 +0000

    Relicense all files to MIT, so people can use this code as inspiration or even as a basis for development of any kind...
    Original commit message from CVS:
    Relicense all files to MIT, so people can use this code as inspiration or
    even as a basis for development of any kind of plugins, not just free ones.
    Same for applications, of course. Add LGPL and MIT copyright files for good
    practice.
    
    (For those unaware, MIT means "do anything you wish", similar to BSD w/o
    advertising but without explicit no-endorsement clause.)

--- gst-app/src/load.c
+++ gst-app/src/load.c
@@ -1,8 +1,29 @@
-/*
- * GStreamer gst-app template
- * load.c : load a pipeline from disk
+/* Copyright 2005 Thomas Vander Stichele <thomas@apestaart.org>
  *
- * Copyright (C) <2005> GStreamer hacker X
+ * load.c : load a pipeline from disk
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * Alternatively, the contents of this file may be used under the
+ * GNU Lesser General Public License Version 2.1 (the "LGPL"), in
+ * which case the following provisions apply instead of the ones
+ * mentioned above:
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
